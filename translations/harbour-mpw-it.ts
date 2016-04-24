5576a427b83c5d0c17f0ea53910c19a611f3ba7c
Andrea Scarpino <me@andreascarpino.it>
2016-04-24 12:31:09 +0200

    Add Italian and Swedish translations

---
+++ translations/harbour-mpw-it.ts
@@ -0,0 +1,103 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="it" version="2.1">
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="40"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="46"/>
+        <source>Copy to clipboard</source>
+        <translation>Copia negli appunti</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="54"/>
+        <source>Clear</source>
+        <translation>Cancella</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="73"/>
+        <source>Site name (e.g. google.com)</source>
+        <translation>Sito web (google.com, ...)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="84"/>
+        <source>Counter</source>
+        <translation>Contatore</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="93"/>
+        <source>Password type</source>
+        <translation>Tipologia password</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="102"/>
+        <source>PIN</source>
+        <translation>PIN</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="126"/>
+        <source>Please fill your name and master password in the Settings page!</source>
+        <translation>Perfavore immetti il tuo nome e password nella pagina Impostazioni!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="101"/>
+        <source>Short</source>
+        <translation>Corta</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="100"/>
+        <source>Basic</source>
+        <translation>Semplice</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="99"/>
+        <source>Medium</source>
+        <translation>Media</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="98"/>
+        <source>Long</source>
+        <translation>Lunga</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="97"/>
+        <source>Maximum</source>
+        <translation>Massima</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="103"/>
+        <source>Name</source>
+        <translation>Nome</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="104"/>
+        <source>Phrase</source>
+        <translation>Frase</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="38"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="45"/>
+        <source>Full name</source>
+        <translation>Nome completo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="51"/>
+        <source>Master password</source>
+        <translation>Master password</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="57"/>
+        <source>Save</source>
+        <translation>Salva</translation>
+    </message>
+</context>
+</TS>
