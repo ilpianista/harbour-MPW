7ad86fd9b2e36128c5352ebacc7c6810ad6ef477
Kristjan Räts <kristjanrats@gmail.com>
2018-02-11 10:42:45 +0000

    Translated using Weblate (Estonian)
    Currently translated at 100.0% (24 of 24 strings)
    
    Translation: Harbour MPW/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-mpw/translations/et/

--- translations/harbour-mpw-et.ts
+++ translations/harbour-mpw-et.ts
@@ -16,7 +16,7 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="107"/>
         <source>Site name (e.g. google.com)</source>
-        <translation></translation>
+        <translation>Saidi nimi (e.g. google.com)</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="146"/>
@@ -26,67 +26,67 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="132"/>
         <source>PIN</source>
-        <translation></translation>
+        <translation>PIN-kood</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="161"/>
         <source>Touch here to set your master password or use the Settings page!</source>
-        <translation></translation>
+        <translation>Puuduta seda, et määrata oma ülemparool või kasuta lehte Seaded!</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="170"/>
         <source>Sites</source>
-        <translation></translation>
+        <translation>Saidid</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="131"/>
         <source>Short</source>
-        <translation></translation>
+        <translation>Lühike</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="130"/>
         <source>Basic</source>
-        <translation></translation>
+        <translation>Lihtne</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="129"/>
         <source>Medium</source>
-        <translation></translation>
+        <translation>Keskmine</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="128"/>
         <source>Long</source>
-        <translation></translation>
+        <translation>Pikk</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="127"/>
         <source>Maximum</source>
-        <translation></translation>
+        <translation>Maksimum</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="58"/>
         <source>Clear sites</source>
-        <translation></translation>
+        <translation>Puhasta saidid</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="73"/>
         <source>Clear password</source>
-        <translation></translation>
+        <translation>Puhasta parool</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="121"/>
         <source>Type</source>
-        <translation></translation>
+        <translation>Tüüp</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="133"/>
         <source>Name</source>
-        <translation></translation>
+        <translation>Nimi</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="134"/>
         <source>Phrase</source>
-        <translation></translation>
+        <translation>Fraas</translation>
     </message>
 </context>
 <context>
@@ -94,27 +94,27 @@
     <message>
         <location filename="../qml/pages/Settings.qml" line="49"/>
         <source>Settings</source>
-        <translation></translation>
+        <translation>Seaded</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="56"/>
         <source>Full name</source>
-        <translation></translation>
+        <translation>Täisnimi</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="64"/>
         <source>Master password</source>
-        <translation></translation>
+        <translation>Ülemparool</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="72"/>
         <source>Algorithm version</source>
-        <translation></translation>
+        <translation>Algoritmi versiooni</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="97"/>
         <source>Generate</source>
-        <translation></translation>
+        <translation>Genereeri</translation>
     </message>
 </context>
 <context>
@@ -122,12 +122,12 @@
     <message>
         <location filename="../qml/pages/SiteDelegate.qml" line="43"/>
         <source>Delete</source>
-        <translation></translation>
+        <translation>Kustuta</translation>
     </message>
     <message>
         <location filename="../qml/pages/SiteDelegate.qml" line="76"/>
         <source>Deleting</source>
-        <translation></translation>
+        <translation>Kustutatakse</translation>
     </message>
 </context>
 </TS>
