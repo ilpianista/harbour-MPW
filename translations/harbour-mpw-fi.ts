30479f126cbc159e969dc9ad7eaef091ed888b76
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2020-12-20 17:57:59 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (24 of 24 strings)
    
    Translation: Harbour MPW/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-mpw/translations/fi/

--- translations/harbour-mpw-fi.ts
+++ translations/harbour-mpw-fi.ts
@@ -6,87 +6,87 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="51"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Asetukset</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="85"/>
         <source>Copy to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopioi leikepöydälle</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="107"/>
         <source>Site name (e.g. google.com)</source>
-        <translation type="unfinished"></translation>
+        <translation>Sivuston nimi (esim. google.com)</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="146"/>
         <source>Counter</source>
-        <translation type="unfinished"></translation>
+        <translation>Laskuri</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="132"/>
         <source>PIN</source>
-        <translation type="unfinished"></translation>
+        <translation>PIN</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="161"/>
         <source>Touch here to set your master password or use the Settings page!</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikkaa tätä asettaaksesi pääsalasanasi tai käytä asetukset-sivua!</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="170"/>
         <source>Sites</source>
-        <translation type="unfinished"></translation>
+        <translation>Sivustot</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="131"/>
         <source>Short</source>
-        <translation type="unfinished"></translation>
+        <translation>Lyhyt</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="130"/>
         <source>Basic</source>
-        <translation type="unfinished"></translation>
+        <translation>Perus</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="129"/>
         <source>Medium</source>
-        <translation type="unfinished"></translation>
+        <translation>Keski</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="128"/>
         <source>Long</source>
-        <translation type="unfinished"></translation>
+        <translation>Pitkä</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="127"/>
         <source>Maximum</source>
-        <translation type="unfinished"></translation>
+        <translation>Enimmäis</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="58"/>
         <source>Clear sites</source>
-        <translation type="unfinished"></translation>
+        <translation>Tyhjennä sivustot</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="73"/>
         <source>Clear password</source>
-        <translation type="unfinished"></translation>
+        <translation>Tyhjennä salasana</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="121"/>
         <source>Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Tyyppi</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="133"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nimi</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="134"/>
         <source>Phrase</source>
-        <translation type="unfinished"></translation>
+        <translation>Lause</translation>
     </message>
 </context>
 <context>
@@ -94,27 +94,27 @@
     <message>
         <location filename="../qml/pages/Settings.qml" line="49"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Asetukset</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="56"/>
         <source>Full name</source>
-        <translation type="unfinished"></translation>
+        <translation>Koko nimi</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="64"/>
         <source>Master password</source>
-        <translation type="unfinished"></translation>
+        <translation>Pääsalasana</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="72"/>
         <source>Algorithm version</source>
-        <translation type="unfinished"></translation>
+        <translation>Algoritmiversio</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="97"/>
         <source>Generate</source>
-        <translation type="unfinished"></translation>
+        <translation>Luo</translation>
     </message>
 </context>
 <context>
@@ -122,12 +122,12 @@
     <message>
         <location filename="../qml/pages/SiteDelegate.qml" line="43"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Poista</translation>
     </message>
     <message>
         <location filename="../qml/pages/SiteDelegate.qml" line="76"/>
         <source>Deleting</source>
-        <translation type="unfinished"></translation>
+        <translation>Poistetaan</translation>
     </message>
 </context>
 </TS>
