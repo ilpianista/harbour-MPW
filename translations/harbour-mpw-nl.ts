d0bd4266739b77cb8395ef3e6a2f7a67b7886e03
Heimen Stoffels <vistausss@fastmail.com>
2026-03-23 10:48:27 +0100

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (24 of 24 strings)
    
    Translation: Harbour MPW/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-mpw/translations/nl/

--- translations/harbour-mpw-nl.ts
+++ translations/harbour-mpw-nl.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="65"/>
         <source>Clearing sites</source>
-        <translation type="unfinished"></translation>
+        <translation>Opschonen van websites</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="79"/>
@@ -31,7 +31,7 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="159"/>
         <source>Tap to set your master password or use the &quot;Settings&quot; page</source>
-        <translation type="unfinished"></translation>
+        <translation>Druk op je hoofdwachtwoord in te stellen of open de instellingen hiervoor</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="126"/>
