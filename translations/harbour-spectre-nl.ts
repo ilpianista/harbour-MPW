139746198188832e506347b4f17c20eae5c0a5fc
Nathan <nthn@unseen.is>
2026-08-29 16:24:57 +0200

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (24 of 24 strings)
    
    Translation: Harbour Spectre/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-spectre/translations/nl/

--- translations/harbour-spectre-nl.ts
+++ translations/harbour-spectre-nl.ts
@@ -31,7 +31,7 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="205"/>
         <source>Tap to set your master password or use the &quot;Settings&quot; page</source>
-        <translation>Druk op je hoofdwachtwoord in te stellen of open de instellingen hiervoor</translation>
+        <translation>Druk om je hoofdwachtwoord in te stellen of open de instellingen hiervoor</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="164"/>
